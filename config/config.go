d3d3def3109aba8484f5426926877fa01131c5a6
Dan Chen <dan.chen@boardware.com>
2023-08-16 17:31:49 +0800

    fixup

--- config/config.go
+++ config/config.go
@@ -7,27 +7,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-func SetPath(path string) {
-	viper.AddConfigPath(path)
-}
-func SetName(name string) {
-	viper.SetConfigName(name)
-}
-func SetType(t string) {
-	viper.SetConfigType(t)
-}
-
-func Init(path, name, t string) {
-	var err error
-	viper.SetConfigName(name) // name of config file (without extension)
-	viper.SetConfigType(t)
-	viper.AddConfigPath(path)  // optionally look for config in the working directory
-	err = viper.ReadInConfig() // Find and read the config file
-	if err != nil {            // Handle errors reading the config file
-		panic(fmt.Errorf("databases fatal error config file: %w", err))
-	}
-}
-
 func init() {
 	var err error
 	viper.SetConfigName(".env") // name of config file (without extension)
