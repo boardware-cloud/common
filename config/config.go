9f86719661d9bd0a1bf5ad6bd4e28367a2b5c978
Dan Chen <dan.chen@boardware.com>
2023-08-17 09:47:21 +0800

    fixup

--- config/config.go
+++ config/config.go
@@ -1,21 +1,16 @@
 package config
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/viper"
 )
 
 func init() {
-	var err error
 	viper.SetConfigName(".env") // name of config file (without extension)
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("./config") // optionally look for config in the working directory
-	err = viper.ReadInConfig()      // Find and read the config file
-	if err != nil {                 // Handle errors reading the config file
-		panic(fmt.Errorf("databases fatal error config file: %w", err))
-	}
+	viper.ReadInConfig()            // Find and read the config file
 }
 
 func GetString(s string) string {
